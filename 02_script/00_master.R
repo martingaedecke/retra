5c4de6cac6ba7ab309f4464a3054d81873ccf7de
martingaedecke <martin.gaedecke@nuffield.ox.ac.uk>
2023-12-14 15:17:25 +0100

    folder setup 2

---
+++ 02_script/00_master.R
@@ -0,0 +1,5 @@
+### This master script has the purpose to update and load packages and
+### set working directories as well as executing seperate do-files
+
+### Install and update packages
+
---
+++ 02_script/00_packages.R
@@ -0,0 +1,3 @@
+### Packages R Script ###
+
+
