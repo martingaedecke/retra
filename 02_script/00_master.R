063ec5dc264f57447bd296f45d4788c86a981427
martingaedecke <martin.gaedecke@nuffield.ox.ac.uk>
2023-12-14 15:19:24 +0100

    updated packages R script and master R script

--- 02_script/00_master.R
+++ 02_script/00_master.R
@@ -1,5 +1,13 @@
 ### This master script has the purpose to update and load packages and
 ### set working directories as well as executing seperate do-files
 
+setwd("C:/Users/maddinjedekhe/Documents/OneDrive - Nexus365/retra")
+
+### Folder setup
+source('00_projectsetup.R')
+
 ### Install and update packages
+source('./02_script/00_packages.R')
+
+
 
